678a9fa53acd110cfd5c9b89412350a4a7f95e99
Elliot Schumacher <elliot.l.schumacher.code@gmail.com>
2022-12-11 12:56:55 -0800

    Information panel displays state info

--- index.js
+++ index.js
@@ -11,24 +11,53 @@ async function init() {
     d3.csv("./data/mass_shootings_shrunk.csv"),
     d3.csv("./data/population.csv")])
     .then(processData)
-    .then(createMap)
     .catch(console.error)
   
     await createMap([stateData, stateList, shootingData, populationData]);
 }
 
-function showDetails(event) {
-  // console.log(event);
+/**
+ * Populates the info panel with state information and the state's mass shooting statistics.
+ * @param {Object} clickedState -  The GeoJSON feature of the state to show info on.
+ */
   // Swap to detail view if not aready there
   if (inMapView) {
     swapViews();
     inMapView = !inMapView;
   }
-  let state = event.properties.stateName;
-  console.log(state);
-  // Update info pane
+  let state = clickedState.properties.stateName;
+
+  // Update info pane header
   let stateLabel = document.querySelector("#info h2");
-  stateLabel.innerHTML = event.properties.name;
+  stateLabel.innerHTML = clickedState.properties.name;
+  
+  // Populate statistics in info panel
+  let {totalIncidents, totalKilled, totalInjuries, population} = calculateStatistics("21", state); 
+  document.getElementById("incident-rate").innerHTML = ((totalIncidents / population) * 1000000).toFixed(1); // rate per million
+  document.getElementById("incident-total").innerHTML = totalIncidents;
+  document.getElementById("fatalities-rate").innerHTML = ((totalKilled / population) * 1000000).toFixed(1); // rate per million
+  document.getElementById("fatalities-total").innerHTML = totalKilled;
+  document.getElementById("injuries-rate").innerHTML = ((totalInjuries / population) * 1000000).toFixed(1); // rate per million
+  document.getElementById("injuries-total").innerHTML = totalInjuries;
+  document.getElementById("population-total").innerHTML = parseInt(population).toLocaleString();
+}
+
+/**
+ * Calculates count and sum information that is displayed in the info panel.
+ * @param {String} year - The last two digits of the year to generate statistics for.
+ * @param {String} state - The name of the state to generate statistics for in lowercase.
+ * @returns {Object} - The number of shootings, the sum of people killed and injured and
+ *                     the state's population.
+ */
+function calculateStatistics(year, state) {
+  let stateShootingData = shootingData[year][state] ? shootingData[year][state] : [];
+  let totalIncidents = stateShootingData.length;
+  let totalKilled = stateShootingData.map(item => parseInt(item["# Killed"]))
+    .reduce((prev, next) => prev + next, 0);
+  let totalInjuries = stateShootingData.map(item => parseInt(item["# Injured"]))
+    .reduce((prev, next) => prev + next, 0);
+  let population = populationData[state]["POPESTIMATE20" + year];
+  return {totalIncidents, totalKilled, totalInjuries, population}
 }
 
 /**
@@ -61,19 +90,19 @@ function getMapHeight() {
 }
 
 function processData([stateData, shootingData, populationData]) {
-  // Process the stateData by making state names lowercase
+  // Add lowercase state names to each state object in state and population data
   for (let state of stateData.features) {
     state.properties.stateName = state.properties.name.toLowerCase();
   }
-
-  // Process the populationData by making state names lowercase
   for (let state of populationData) {
     state.stateName = state.NAME.toLowerCase();
   }
+
+  // Transform population data from an array to an object
   let mappedPopulationData = populationData.map((item) => ({[item.stateName]: item}));
   let processedPopulationData = Object.assign({}, ...mappedPopulationData);
 
-  // Process the shootingData by making state names lowercase and grouping data by year
+  // Add lowercase state names lowercase and grouping data by year and then state
   let shootingByYear = {};
   for (let shooting of shootingData) {
     shooting.stateName = shooting.State.toLowerCase();
@@ -90,8 +119,6 @@ function processData([stateData, shootingData, populationData]) {
 
   // Create list of states
   let stateList = stateData.features.map((d) => { return d.properties.stateName.toLowerCase() })
-  // console.log("stateList", stateList);
-  // console.log("shootingByYear", shootingByYear);
   return [stateData, stateList, shootingByYear, processedPopulationData];
 }
 
@@ -106,6 +133,7 @@ function createMap([stateData, stateList, shootingData, populationData]) {
     preexistingSVG.remove();
   }
 
+  // Setup map properties
   let mapHeight = getMapHeight();
   let mapWidth = getMapWidth();
   let projection = d3.geoAlbersUsa();
@@ -127,8 +155,7 @@ function createMap([stateData, stateList, shootingData, populationData]) {
       .attr("d", path)
       .style("fill", function (d) {
         let state = d.properties.stateName;
-        let shootingRate = getShootingRate("21", state, shootingData, populationData);
-        return colorScale(shootingRate);
+        return colorScale(getShootingRate("21", state, shootingData, populationData));
       })
       .style("stroke", "#000")
       .on("click", showDetails)
